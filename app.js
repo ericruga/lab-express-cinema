c88cdfbc541f2f7423b3aba133d307c6fce80df4
ericruga <ericruga@users.noreply.github.com>
2022-07-10 18:12:34 +0200

    Final iterations

--- app.js
+++ app.js
@@ -19,17 +19,14 @@ const app = express();
 require('./config')(app);
 
 // default value for title local
-const projectName = 'lab-express-cinema';
-const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
+//const projectName = 'lab-express-cinema';
+//const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
-
-app.get('/movies', (req, res) => {
-    )
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,35 @@
 const express = require('express');
 const router = express.Router();
-
-/* GET home page */
+const Movies = require('../models/Movie.model');
 router.get('/', (req, res, next) => res.render('index'));
+
+outer.get('/movies', (req, res) => {
+    Movies.find()
+    .then((movies) => {
+    
+    console.log(movies, "estas son las peliculas");
+     res.render("movies.hbs", { movies });
+    
+    })
+    .catch((err)=>{
+        console.log(err)
+    });
+    
+    
+    router.get("/details/:id",(req, res) => {
+    const id = req.params.id
+    
+    Movies.findById(id)
+      .then((movie) => {
+          console.log(movie)
+        res.render("details.hbs",  movie );
+      })
+    
+      .catch((err) => {
+        console.log(err);
+      });    
+    })    
+});
+
 
 module.exports = router;
